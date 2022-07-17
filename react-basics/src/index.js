b532d9251fae8b4fd4c79175954e6a989a3ee3bf
Darryl Le Roux <devdarryl@outlook.com>
2022-07-17 19:40:51 +0200

    Added img source

--- react-basics/src/index.js
+++ react-basics/src/index.js
@@ -1,9 +1,36 @@
 import React from "react";
 import ReactDom from "react-dom/client";
 
-function Greeting() {
-  return <h1>Hello</h1>;
+function Booklist() {
+  return (
+    <section>
+      <Book />
+    </section>
+  );
 }
 
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => {
+  <img
+    src="https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UY218_.jpg"
+    alt=""
+  />;
+};
+
+const Title = () => {
+  return <h1>Love you to the moon and back</h1>;
+};
+
+const Author = () => <h4>Amelia Hepworth</h4>;
+
 const root = ReactDom.createRoot(document.getElementById("root"));
-root.render(<Greeting />);
+root.render(<Booklist />);
