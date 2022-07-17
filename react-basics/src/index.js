f65ecbbc636ad592b4a58aa8f61c138d5945eff0
Darryl Le Roux <devdarryl@outlook.com>
2022-07-17 20:40:13 +0200

    Removed components in favor of one

--- react-basics/src/index.js
+++ react-basics/src/index.js
@@ -17,26 +17,15 @@ function Booklist() {
 const Book = () => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UL320_.jpg"
+        alt="Book"
+      />
+      <h1>Love you to the moon and back</h1>
+      <h4>Amelia Hepworth</h4>
     </article>
   );
 };
-
-const Image = () => {
-  return (
-    <img
-      src="https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UL320_.jpg"
-      alt="Book"
-    />
-  );
-};
-const Title = () => {
-  return <h1>Love you to the moon and back</h1>;
-};
-
-const Author = () => <h4>Amelia Hepworth</h4>;
 
 const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(<Booklist />);
