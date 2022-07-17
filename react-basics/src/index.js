47ab61033745fa3feb4ff81240d0319642c0fe0f
Darryl Le Roux <devdarryl@outlook.com>
2022-07-17 20:45:52 +0200

    Added js in jsx

--- react-basics/src/index.js
+++ react-basics/src/index.js
@@ -15,14 +15,16 @@ function Booklist() {
 }
 
 const Book = () => {
+  const author = "Amelia Hepworth";
+  const title = "Love you to the moon and back";
   return (
     <article className="book">
       <img
         src="https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UL320_.jpg"
         alt="Book"
       />
-      <h1>Love you to the moon and back</h1>
-      <h4>Amelia Hepworth</h4>
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 };
