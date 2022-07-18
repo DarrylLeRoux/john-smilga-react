c93c4047114d55fa244101deb68d647b810148ab
Darryl Le Roux <devdarryl@outlook.com>
2022-07-18 21:41:33 +0200

    added props

--- react-basics/src/index.js
+++ react-basics/src/index.js
@@ -4,27 +4,42 @@ import ReactDom from "react-dom/client";
 import "./index.css";
 
 // Setup variables
-const author = "Amelia Hepworth";
-const title = "Love you to the moon and back";
-const img = "https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UL320_.jpg";
+const firstBook = {
+  author: "Amelia Hepworth",
+  title: "I Love You to the Moon and Back",
+  img: "https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UL320_.jpg",
+};
+
+const secondBook = {
+  author: "Shannon Olsen",
+  title: "Our Class is a Family",
+  img: "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UL320_.jpg",
+};
 
 function Booklist() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
-      <img src={img} alt="book" />
-      <h1>{title}</h1>
-      <h4>{author}</h4>
+      <img src={props.img} alt="book" />
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
     </article>
   );
 };
