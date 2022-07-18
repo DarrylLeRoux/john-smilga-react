011ad3b7499d81b6a33f265a4610c9caeb740331
Darryl Le Roux <devdarryl@outlook.com>
2022-07-18 22:21:16 +0200

    Added map method

--- react-basics/src/index.js
+++ react-basics/src/index.js
@@ -4,50 +4,44 @@ import ReactDom from "react-dom/client";
 import "./index.css";
 
 // Setup variables
-const firstBook = {
-  author: "Amelia Hepworth",
-  title: "I Love You to the Moon and Back",
-  img: "https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UL320_.jpg",
-};
+const books = [
+  {
+    author: "Amelia Hepworth",
+    title: "I Love You to the Moon and Back",
+    img: "https://m.media-amazon.com/images/I/8144Vic9C5L._AC_UL320_.jpg",
+  },
 
-const secondBook = {
-  author: "Shannon Olsen",
-  title: "Our Class is a Family",
-  img: "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UL320_.jpg",
-};
+  {
+    author: "Shannon Olsen",
+    title: "Our Class is a Family",
+    img: "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UL320_.jpg",
+  },
+  {
+    author: "Emily Winfield Martin",
+    title: "The Wonderful Things you Will be",
+    img: "https://m.media-amazon.com/images/I/91HHxxtA1wL._AC_UL320_.jpg",
+  },
+];
 
 function Booklist() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit enim
-          dolorem reiciendis accusantium molestiae voluptatibus, eligendi
-          recusandae veritatis deserunt dolores.
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        const { img, title, author } = book;
+        return <Book book={book}></Book>;
+      })}
     </section>
   );
 }
 
 const Book = (props) => {
   // props destructuring
-  const { img, title, author } = props;
+  const { img, title, author } = props.book;
   return (
     <article className="book">
       <img src={img} alt="book" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {props.children}
     </article>
   );
 };
