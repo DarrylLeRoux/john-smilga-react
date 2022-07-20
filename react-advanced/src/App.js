df57adbe2dbeb1278275070088a62765044a45f6
Darryl Le Roux <devdarryl@outlook.com>
2022-07-20 21:27:10 +0200

    Added two variations of changing state of an object

--- react-advanced/src/App.js
+++ react-advanced/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Setup from "./tutorial/1-useState/setup/3-useState-array";
+import Setup from "./tutorial/1-useState/setup/4-useState-object";
 function App() {
   return (
     <div className="container">
-      <h2>
+      <h1>
         <Setup />
-      </h2>
+      </h1>
     </div>
   );
 }
--- react-advanced/src/tutorial/1-useState/setup/4-useState-object.js
+++ react-advanced/src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "peter",
+    age: 24,
+    message: "random message",
+  });
+
+  const [name, setName] = useState("Peter");
+  const [age, setAge] = useState(23);
+  const [message, setMessage] = useState("Hi there");
+
+  const changeMessage = () => {
+    setName("Darryl");
+    setAge(39);
+    setMessage("Darryl is super cool");
+  };
+
+  const changeMessage2 = () => {
+    setPerson({ name: "John", age: 39, message: "Hi there again" });
+  };
+
+  return (
+    <>
+      <h4>{name}</h4>
+      <h4>{age}</h4>
+      <h4>{message}</h4>
+      <button className="btn" onClick={changeMessage}>
+        Change Message
+      </button>
+
+      <h4>{person.name}</h4>
+      <h4>{person.age}</h4>
+      <h4>{person.message}</h4>
+      <button className="btn" onClick={changeMessage2}>
+        Change Message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
