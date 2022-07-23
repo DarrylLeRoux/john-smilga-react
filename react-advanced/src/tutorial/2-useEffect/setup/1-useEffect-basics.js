8412888f9c22748ba3cf671046f6eb36bd843197
Darryl Le Roux <devdarryl@outlook.com>
2022-07-23 21:49:53 +0200

    added ternary to useEffect

--- react-advanced/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ react-advanced/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    value === 0
+      ? (document.title = "No new messages")
+      : (document.title = `New Messages ${value}`);
+  });
+
+  const handleClick = () => {
+    setValue(value + 1);
+  };
+  useEffect(() => {});
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className="btn" onClick={handleClick}>
+        Increase
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
