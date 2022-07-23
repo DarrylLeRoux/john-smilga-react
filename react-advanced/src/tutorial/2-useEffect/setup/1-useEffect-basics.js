65e8fa9fe713d2061a8487f701b680beb03d383c
Darryl Le Roux <devdarryl@outlook.com>
2022-07-23 21:58:00 +0200

    Added dependancy for useEffect

--- react-advanced/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ react-advanced/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -6,10 +6,13 @@ const UseEffectBasics = () => {
   const [value, setValue] = useState(0);
 
   useEffect(() => {
-    value === 0
-      ? (document.title = document.title)
-      : (document.title = `New Messages ${value}`);
-  });
+    console.log("call useffect");
+    if (value >= 0) {
+      document.title = `New Messages ${value}`;
+    }
+    // using an empty dependancy will only run the useEffect when it renders for the first time
+    // using a dependancy (value of useState) will trigger the useEffect each time it is triggered
+  }, []);
 
   const handleClick = () => {
     setValue(value + 1);
