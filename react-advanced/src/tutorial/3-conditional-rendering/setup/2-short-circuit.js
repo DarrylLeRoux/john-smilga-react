f7b442060cac8658e2ca8e661a1d133ee0c95f47
Darryl Le Roux <devdarryl@outlook.com>
2022-07-25 21:35:07 +0200

    toggle state

--- react-advanced/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ react-advanced/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
-
-  return <h2>short circuit</h2>;
+  const [text, setText] = useState("peter");
+  const [isError, setIsError] = useState(false);
+  return (
+    <>
+      {/* if text is false it will render the "john doe" */}
+      <h1>{text || "john doe"}</h1>
+      {/* if text is true, then it will render peter and hello world */}
+      <button
+        className="btn"
+        onClick={() => {
+          setIsError(!isError);
+        }}
+      >
+        toggle error
+      </button>
+      {isError && <h1>Error...</h1>}
+    </>
+  );
 };
 
 export default ShortCircuit;
